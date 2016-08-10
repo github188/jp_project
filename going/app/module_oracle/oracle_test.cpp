5596622d365e98d3d5b5813892e32916335cd81b
Goinggit <451080373@qq.com>
2016-08-10 20:40:38 +0800

    gogogo

--- going/app/module_oracle/oracle_test.cpp
+++ going/app/module_oracle/oracle_test.cpp
@@ -39,6 +39,7 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
+/*********************************************/
 #include "../../module/oracle/myoracle.h"
 #include "ocilib.h"
 #include "stdio.h"
